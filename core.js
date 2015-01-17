08b93227da0deb0db95472ae5761d813f21ace2e
tom <caolvchong@gmail.com>
2015-01-17 11:41:56 +0800

    支持参数解析返回

--- core.js
+++ core.js
@@ -1,6 +1,12 @@
 var fs = require('fs');
 var util = require('util');
 
+var reg = {
+    expression: /{{(.*?)}}/g,
+    split: /\s*\|\s*/,
+    json: /.+\.json$/
+}
+
 function readAPIs(app, path, config) {
     var delay = config.delay;
     var status = config.status;
@@ -10,30 +16,52 @@ function readAPIs(app, path, config) {
         .forEach(function (file) {
             var p = path + '/' + file;
             var stat = fs.statSync(p);
-            if (stat.isFile() && file.match(/.+\.json$/g) !== null) {
+            if (stat.isFile() && file.match(reg.json) !== null) {
                 var apis = require(p);
                 if (!util.isArray(apis)) {
                     apis = [apis];
                 }
                 apis.forEach(function (api) {
                     app[api.method](api.url, function*() {
-                        if(api.delay) {
+                        if (api.delay) {
                             delay = api.delay;
                         }
-                        if(delay) {
+                        if (delay) {
                             yield function (done) {
                                 setTimeout(done, delay);
                             };
                         }
 
-                        if(api.status) {
+                        if (api.status) {
                             status = api.status;
                         }
-                        if(status && status !== 200) {
+                        if (status && status !== 200) {
                             this.status = status;
                             this.body = api.error || {message: '默认异常'};
                         } else {
-                            this.body = api.response;
+                            var res = api.response || {};
+
+                            (function (res) {
+                                for (var key in res) {
+                                    var val = res[key];
+                                    if (typeof val === 'string' && val.match(reg.expression)) {
+                                        var arr = key.split(reg.split);
+                                        if (arr[1]) {
+                                            res[arr[0]] = res[key];
+                                            delete res[key];
+                                            key = arr[0];
+                                        }
+                                        res[key] = val.replace(reg.expression, function (match, expression) {
+                                            return eval(expression);
+                                        }.bind(this));
+                                        if (arr[1] === 'number') {
+                                            res[key] = +res[key];
+                                        }
+                                    }
+                                }
+                            }.bind(this))(res);
+
+                            this.body = res;
                         }
                     });
                 });
