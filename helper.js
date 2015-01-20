e61f207eb71608bf50aec0b8c660b230a31ca726
tom <caolvchong@gmail.com>
2015-01-20 23:55:09 +0800

    提供JSON深拷贝

--- helper.js
+++ helper.js
@@ -5,7 +5,7 @@ var pagination = function(store, params) {
     var offset = +params.offset || 0;
     var limit = +params.limit || 10;
     var data = [];
-    var next = Math.min(offset + limit, store.length - 1);
+    var next = Math.min(offset + limit, store.length);
     for(var i = offset; i < next; i++) {
         data.push(store[i]);
     }
@@ -18,5 +18,10 @@ var pagination = function(store, params) {
         }
     }
 };
-
 exports.pagination = pagination;
+
+var JSONCopy = function(json) {
+    var copy = JSON.parse(JSON.stringify(json));
+    return copy;
+};
+exports.JSONCopy = JSONCopy;
